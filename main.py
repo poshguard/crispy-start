19d3679ca62c9cb78f3ebe5d774d49fdd4e79c2c
Ulugbek Turaev <u.turayev1@gmail.com>
2023-01-16 21:04:02 -0700

    This is our project initial state.

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print('git')
-print("hello world")
+import pandas as pd
+
+series = pd.Series(["BMW", "Toyota", "Honda"])
+print(series)
+
+colours = pd.Series(["Red", "Blue", "White"])
+print(colours)
+
+car_data = pd.DataFrame({"Car make": series, "Colour": colours})
+print(car_data)
+
